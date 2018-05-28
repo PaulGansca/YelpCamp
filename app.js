87f06426d66ac85d80fe6c160532a451b1a387da
Gansca Paul Mihai <gansca.paul@gmail.com>
2018-05-28 12:55:10 +0000

    Added nested routes for comments, each post has its own comments now. Made separate directories for the models.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
+var Comment = require("./models/comment");
 var seedDB = require("./seeds");
 
 seedDB();
@@ -22,14 +23,14 @@ app.get("/campgrounds", function(req, res){
         if(err){
             console.log(err);
         } else {
-            res.render("index", {campgrounds:allCampgrounds});
+            res.render("campgrounds/index", {campgrounds:allCampgrounds});
         }
     });
 });
 
 //NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res) {
-    res.render("new");
+    res.render("campgrounds/new");
 });
 
 //SHOW - shows more info about one campground
@@ -41,7 +42,7 @@ app.get("/campgrounds/:id", function(req, res) {
        } else {
            console.log(foundCampground);
            //show more information about that item
-           res.render("show", {campground: foundCampground});
+           res.render("campgrounds/show", {campground: foundCampground});
        }
     });
 });
@@ -62,6 +63,43 @@ app.post("/campgrounds", function(req, res){
         }
     });
 })
+
+//COMMENTS ROUTES
+
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+    //find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: campground});
+        }
+    });
+    
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    //lookup campground using ID
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+        }
+    })
+    //create new comment
+    //connect new comment to campground
+    //redirect to campground show page
+});
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
