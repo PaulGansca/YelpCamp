7a08355245eee44af0c45a521a7570e1710d62b3
Gansca Paul Mihai <gansca.paul@gmail.com>
2018-05-28 15:47:47 +0000

    Styled show pages, for individual campgrounds

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var seedDB = require("./seeds");
 seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
 // LANDING PAGE
