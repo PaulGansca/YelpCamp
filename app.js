1674b51d1127533282dd48ab00632deac62f95a0
Gansca Paul Mihai <gansca.paul@gmail.com>
2018-05-30 23:42:31 +0000

    Added login and logout functionality, only logged in Users can add comments

--- app.js
+++ app.js
@@ -27,6 +27,12 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//midlleware, dry up code by calling this on every route, so that currentUser is always defined
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   next();
+});
+
 // LANDING PAGE
 app.get("/", function(req, res){
     res.render("landing");
@@ -39,7 +45,7 @@ app.get("/campgrounds", function(req, res){
         if(err){
             console.log(err);
         } else {
-            res.render("campgrounds/index", {campgrounds:allCampgrounds});
+            res.render("campgrounds/index", {campgrounds: allCampgrounds});
         }
     });
 });
@@ -82,7 +88,7 @@ app.post("/campgrounds", function(req, res){
 
 //COMMENTS ROUTES
 
-app.get("/campgrounds/:id/comments/new", function(req, res) {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
     //find campground by id
     Campground.findById(req.params.id, function(err, campground){
         if(err){
@@ -94,7 +100,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
     
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
     //lookup campground using ID
     Campground.findById(req.params.id, function(err, campground){
         if(err){
@@ -137,6 +143,32 @@ app.post("/register", function(req, res) {
     });
 });
 
+//show login form
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+//handling login logic
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req, res) {
+    
+});
+
+
+//logout route
+app.get("/logout", function(req, res) {
+   req.logout();
+   res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Yelp camp server is running");
